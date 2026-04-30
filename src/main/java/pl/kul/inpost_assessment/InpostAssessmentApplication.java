07907e4699a6dc9683855caaa6811b6c4fdb675c
adam <adamnowakhomm@gmail.com>
2026-04-30 11:54:20 +0200

    Initial commit

---
+++ src/main/java/pl/kul/inpost_assessment/InpostAssessmentApplication.java
@@ -0,0 +1,13 @@
+package pl.kul.inpost_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpostAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InpostAssessmentApplication.class, args);
+    }
+
+}
