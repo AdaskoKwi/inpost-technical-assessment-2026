7b25da3f1bdbe6f3ad525684160f9e20326201fc
adam <adamnowakhomm@gmail.com>
2026-05-01 12:19:58 +0200

    Updated the ApiClient to save point info to db, minor config change.

--- src/main/java/pl/kul/inpost_assessment/InpostAssessmentApplication.java
+++ src/main/java/pl/kul/inpost_assessment/InpostAssessmentApplication.java
@@ -1,7 +1,13 @@
 package pl.kul.inpost_assessment;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.kul.inpost_assessment.network.ApiClient;
+
+import java.net.http.HttpClient;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class InpostAssessmentApplication {
@@ -10,4 +16,16 @@ public class InpostAssessmentApplication {
         SpringApplication.run(InpostAssessmentApplication.class, args);
     }
 
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .executor(Executors.newVirtualThreadPerTaskExecutor())
+                .build();
+    }
+
+    @Bean
+    public CommandLineRunner run(ApiClient apiClient) {
+        return args -> apiClient.fetchParcelLockers();
+    }
 }
