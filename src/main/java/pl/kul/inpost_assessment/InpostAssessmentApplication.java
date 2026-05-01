dcdcd41663fb42c9d76adc5fbc89b4be4ca0617e
adam <adamnowakhomm@gmail.com>
2026-05-01 12:43:20 +0200

    Created ScheduledTasks class for CRON jobs updating db every hour.

--- src/main/java/pl/kul/inpost_assessment/InpostAssessmentApplication.java
+++ src/main/java/pl/kul/inpost_assessment/InpostAssessmentApplication.java
@@ -1,15 +1,15 @@
 package pl.kul.inpost_assessment;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import pl.kul.inpost_assessment.network.ApiClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.net.http.HttpClient;
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@EnableScheduling
 public class InpostAssessmentApplication {
 
     public static void main(String[] args) {
@@ -22,10 +22,5 @@ public class InpostAssessmentApplication {
                 .version(HttpClient.Version.HTTP_2)
                 .executor(Executors.newVirtualThreadPerTaskExecutor())
                 .build();
-    }
-
-    @Bean
-    public CommandLineRunner run(ApiClient apiClient) {
-        return args -> apiClient.fetchParcelLockers();
     }
 }
